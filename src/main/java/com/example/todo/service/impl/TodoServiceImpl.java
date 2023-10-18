41e13b181cc0b6f9ca314fea05ca443cf72e6d20
Douglas Nhunzvi <dougiedj@gmail.com>
2023-10-18 16:05:20 +0200

    added mapping between Todo and TodoDto

--- src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
+++ src/main/java/com/example/todo/service/impl/TodoServiceImpl.java
@@ -5,6 +5,7 @@ import com.example.todo.entity.Todo;
 import com.example.todo.repository.TodoRepository;
 import com.example.todo.service.TodoService;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,25 +14,28 @@ public class TodoServiceImpl implements TodoService {
 
     private TodoRepository todoRepository;
 
+    private ModelMapper modelMapper;
+
     @Override
     public TodoDto addTodo(TodoDto todoDto) {
         // convert TodoDto into TodoJPA entity
-        Todo todo = new Todo();
+        /*Todo todo = new Todo();
         todo.setTitle( todoDto.getTitle());
         todo.setDescription( todoDto.getDescription());
         todo.setCompleted( todoDto.isCompleted());
-
+*/
+        Todo todo = modelMapper.map(todoDto, Todo.class);
         // Todo JPA entity
         Todo savedTodo = todoRepository.save(todo);
 
         // convert saved Todo JPA entity object into TodoDto object
-        TodoDto savedTodoDto = new TodoDto();
+       /* TodoDto savedTodoDto = new TodoDto();
         savedTodoDto.setTitle(savedTodo.getTitle());
         savedTodoDto.setId(savedTodo.getId());
         savedTodoDto.setDescription(savedTodo.getDescription());
-        savedTodoDto.setCompleted(savedTodo.isCompleted());
+        savedTodoDto.setCompleted(savedTodo.isCompleted());*/
 
-        return savedTodoDto;
+        return modelMapper.map(savedTodo, TodoDto.class);
 
     }
 }
